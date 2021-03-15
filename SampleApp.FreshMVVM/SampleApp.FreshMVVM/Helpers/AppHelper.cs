68fb32328f9886e7174a2815c4ef8716f2564eeb
Virendra <virendra.thakur31@gmail.com>
2021-03-15 19:11:30 +0530

    Helpers added.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Helpers/AppHelper.cs
@@ -0,0 +1,30 @@
+﻿using FreshMvvm;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SampleApp.FreshMVVM.Helpers
+{
+    public class AppHelper
+    {
+        private static FreshMasterDetailNavigationContainer masterDetailNav;
+
+        public static bool MenuIsPresented
+        {
+            get
+            {
+                return masterDetailNav.IsPresented;
+            }
+            set
+            {
+                masterDetailNav.IsPresented = value;
+            }
+        }
+
+        public static void InitializeAndShowMasterDetailPage()
+        {
+           
+        }
+    }
+}
---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Helpers/AppSettings.cs
@@ -0,0 +1,33 @@
+﻿using Plugin.Settings;
+using Plugin.Settings.Abstractions;
+
+namespace SampleApp.FreshMVVM.Helpers
+{
+    public class AppSettings
+    {
+        private const string DefaultUserObjectId = "";
+
+        public static ISettings Settings
+        {
+            get
+            {
+                if (CrossSettings.IsSupported)
+                    return CrossSettings.Current;
+
+                return null;
+            }
+        }
+
+        public static bool IsUserLoggedIn
+        {
+            get
+            {
+                return Settings.GetValueOrDefault(nameof(IsUserLoggedIn), false);
+            }
+            set
+            {
+                Settings.AddOrUpdateValue(nameof(IsUserLoggedIn), value);
+            }
+        }
+    }
+}
