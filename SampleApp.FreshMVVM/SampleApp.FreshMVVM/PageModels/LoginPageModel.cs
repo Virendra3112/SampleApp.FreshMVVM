e35dabdc5a331473923c33e6d179d22ff5e82539
Virendra <virendra.thakur31@gmail.com>
2021-03-17 23:27:14 +0530

    LoginPageModel updated.

--- SampleApp.FreshMVVM/SampleApp.FreshMVVM/PageModels/LoginPageModel.cs
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/PageModels/LoginPageModel.cs
@@ -1,14 +1,38 @@
-﻿using System;
+﻿using SampleApp.FreshMVVM.Helpers;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
 
 namespace SampleApp.FreshMVVM.PageModels
 {
     public class LoginPageModel : BasePageModel
     {
+        private string _email;
+        public string Email
+        {
+            get { return _email; }
+            set { SetPropertyValue(ref _email, value); }
+        }
+
+        private string _password;
+        public string Password
+        {
+            get { return _password; }
+            set { SetPropertyValue(ref _password, value); }
+        }
+
+        public ICommand LoginCommand { get; set; }
+
         public LoginPageModel()
         {
+            LoginCommand = new Command(OnLoginButtonClicked);
+        }
 
+        private void OnLoginButtonClicked(object obj)
+        {
+            AppHelper.InitializeAndShowMasterDetailPage();
         }
     }
 }
