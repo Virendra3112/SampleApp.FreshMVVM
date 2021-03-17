79c8ee74ea5f916dd97a8c1f18a0ae0dc3bda711
Virendra <virendra.thakur31@gmail.com>
2021-03-18 00:03:24 +0530

    DrawerMenuPage updated.

--- SampleApp.FreshMVVM/SampleApp.FreshMVVM/Pages/DrawerMenuPage.xaml.cs
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Pages/DrawerMenuPage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SampleApp.FreshMVVM.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -15,6 +16,10 @@ namespace SampleApp.FreshMVVM.Pages
         public DrawerMenuPage()
         {
             InitializeComponent();
+
+
         }
+
+       
     }
 }
