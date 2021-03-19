dc872f4c85d49a60884e2cd32b904c49a29dfe87
Virendra <virendra.thakur31@gmail.com>
2021-03-19 21:56:01 +0530

    ColourStyles updated.

--- SampleApp.FreshMVVM/SampleApp.FreshMVVM/Resources/ColourStyles.xaml.cs
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Resources/ColourStyles.xaml.cs
@@ -1,16 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace SampleApp.FreshMVVM.Resources
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class ColourStyles : ContentView
+    public partial class ColourStyles : ResourceDictionary
     {
         public ColourStyles()
         {
