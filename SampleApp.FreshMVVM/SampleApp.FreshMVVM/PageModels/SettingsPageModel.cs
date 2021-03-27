26d42e619d99d43435b653acd0c1989389b7c159
Virendra <virendra.thakur31@gmail.com>
2021-03-27 13:47:09 +0530

    SettingsPageModel updated.

--- SampleApp.FreshMVVM/SampleApp.FreshMVVM/PageModels/SettingsPageModel.cs
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/PageModels/SettingsPageModel.cs
@@ -1,6 +1,69 @@
-﻿namespace SampleApp.FreshMVVM.PageModels
+﻿using SampleApp.FreshMVVM.Models;
+using System;
+using System.Collections.ObjectModel;
+
+namespace SampleApp.FreshMVVM.PageModels
 {
     public class SettingsPageModel : BasePageModel
     {
+        private ObservableCollection<MenuItems> _settingsMenuList;
+        public ObservableCollection<MenuItems> SettingsMenuList
+        {
+            get
+            {
+                return _settingsMenuList;
+            }
+            set
+            {
+                _settingsMenuList = value;
+                RaisePropertyChanged();
+            }
+        }
+
+
+
+        private MenuItems _selectedSettingsItem = null;
+        public MenuItems SelectedSettingsItem
+        {
+            get { return _selectedSettingsItem; }
+            set
+            {
+                _selectedSettingsItem = value;
+                if (value != null)
+                {
+                    ExecuteOperation(_selectedSettingsItem);
+                }
+
+            }
+        }
+        public SettingsPageModel()
+        {
+
+        }
+
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+        }
+
+
+        protected override void ViewIsAppearing(object sender, EventArgs e)
+        {
+            base.ViewIsAppearing(sender, e);
+
+            _settingsMenuList = new ObservableCollection<MenuItems>();
+
+            _settingsMenuList.Add(new MenuItems { Id = 1, Name = "Set Theme" });
+        }
+
+        protected override void ViewIsDisappearing(object sender, EventArgs e)
+        {
+            base.ViewIsDisappearing(sender, e);
+        }
+
+        private void ExecuteOperation(MenuItems menuItems)
+        {
+
+        }
     }
 }
