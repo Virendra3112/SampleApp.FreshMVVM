76f6b24a486a2ed1b7f8566445e00fb95c7c2441
Virendra <virendra.thakur31@gmail.com>
2021-03-27 13:40:52 +0530

    EntryValidationBehaviour updated.

--- SampleApp.FreshMVVM/SampleApp.FreshMVVM/Behaviours/EntryValidationBehaviour.cs
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Behaviours/EntryValidationBehaviour.cs
@@ -1,10 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using Xamarin.Forms;
 
 namespace SampleApp.FreshMVVM.Behaviours
 {
-    class EntryValidationBehaviour
+    public class EntryValidationBehaviour : BehaviorBase<Entry>
     {
+        #region StaticFields
+        public static readonly BindableProperty IsValidProperty = BindableProperty.Create(nameof(IsValid), typeof(bool), typeof(EntryValidationBehaviour), true, BindingMode.Default, null, (bindable, oldValue, newValue) => OnIsValidChanged(bindable, newValue));
+        #endregion
+        #region Properties
+        public bool IsValid
+        {
+            get
+            {
+                return (bool)GetValue(IsValidProperty);
+            }
+            set
+            {
+                SetValue(IsValidProperty, value);
+            }
+        }
+        #endregion
+        #region StaticMethods
+        private static void OnIsValidChanged(BindableObject bindable, object newValue)
+        {
+            if (bindable is EntryValidationBehaviour IsValidBehavior &&
+                 newValue is bool IsValid)
+            {
+                IsValidBehavior.AssociatedObject.PlaceholderColor = IsValid ? Color.Default : Color.Red;
+            }
+        }
+
+        #endregion
     }
 }
