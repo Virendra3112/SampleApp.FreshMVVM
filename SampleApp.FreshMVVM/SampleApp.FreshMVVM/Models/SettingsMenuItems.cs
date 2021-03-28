31cf3a7c0fa3152c16c47c68358f549595aa845a
Virendra <virendra.thakur31@gmail.com>
2021-03-28 12:46:02 +0530

    SettingsMenuItems added

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Models/SettingsMenuItems.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleApp.FreshMVVM.Models
+{
+    public class SettingsMenuItems
+    {
+        public int Id { get; set; }
+        public string MenuName { get; set; }
+        public string CurrentMenuSetting { get; set; }
+    }
+}
