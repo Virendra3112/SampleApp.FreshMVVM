e645bade17f74b637f7f5f39b65f778ca83e2220
Virendra <virendra.thakur31@gmail.com>
2021-03-28 13:35:48 +0530

    Settings model updated.

--- SampleApp.FreshMVVM/SampleApp.FreshMVVM/Models/SettingsMenuItems.cs
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Models/SettingsMenuItems.cs
@@ -1,9 +1,11 @@
-﻿namespace SampleApp.FreshMVVM.Models
+﻿using SampleApp.FreshMVVM.Models.Enum;
+
+namespace SampleApp.FreshMVVM.Models
 {
     public class SettingsMenuItems
     {
         public int Id { get; set; }
-        public string MenuName { get; set; }
+        public SettingsMenuEnum MenuName { get; set; }
         public string CurrentMenuSetting { get; set; }
     }
 }
