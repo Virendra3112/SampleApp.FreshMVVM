409a363b320df91c3de238989683b9a3d30c9ad1
Virendra <virendra.thakur31@gmail.com>
2021-03-31 19:59:50 +0530

    ScrollableTabs added.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/CustomControls/ScrollableTabs.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SampleApp.FreshMVVM.CustomControls
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ScrollableTabs : ContentView
+    {
+        public ScrollableTabs()
+        {
+            InitializeComponent();
+        }
+    }
+}
