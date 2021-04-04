edfc0dd66b56f9e586229798a5e0d749f52e5d33
Virendra <virendra.thakur31@gmail.com>
2021-04-04 20:53:08 +0530

    IAppFileHelper added.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Interfaces/IAppFileHelper.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleApp.FreshMVVM.Interfaces
+{
+    public interface IAppFileHelper
+    {
+        bool? IsPreferredLanguageArabic();
+
+        void SaveUserLanguagePreference(bool status);
+    }
+}
