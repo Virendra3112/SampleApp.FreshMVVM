8132359cd032f3938bd0ed68b0d4e5ea952936d0
Virendra <virendra.thakur31@gmail.com>
2021-04-04 20:58:52 +0530

    AppLocale added for iOS.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM.iOS/CustomRendrers/AppLocale.cs
@@ -0,0 +1,36 @@
+﻿using Foundation;
+using SampleApp.FreshMVVM.Interfaces;
+using SampleApp.FreshMVVM.iOS.CustomRendrers;
+using System.Globalization;
+using System.Threading;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AppLocale))]
+namespace SampleApp.FreshMVVM.iOS.CustomRendrers
+{
+    public class AppLocale : IAppLocale
+    {
+        public CultureInfo GetCurrentCultureInfo()
+        {
+            var netLanguage = "en";
+            if (NSLocale.PreferredLanguages.Length > 0)
+                netLanguage = NSLocale.PreferredLanguages[0];
+            System.Globalization.CultureInfo ci = null;
+            try
+            {
+                ci = new System.Globalization.CultureInfo(netLanguage);
+            }
+            catch (CultureNotFoundException ex)
+            {
+                ci = new System.Globalization.CultureInfo("en");//default to en
+            }
+
+            return ci;
+        }
+
+        public void SetLocale(CultureInfo ci)
+        {
+            Thread.CurrentThread.CurrentUICulture = ci;
+        }
+    }
+}
