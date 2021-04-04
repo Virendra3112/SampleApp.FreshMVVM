1f77c837cf02a862be79e58eba3c8b62409a1f1a
Virendra <virendra.thakur31@gmail.com>
2021-04-04 20:56:36 +0530

    AppLocale added for android.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM.Android/CustomRendrers/AppLocale.cs
@@ -0,0 +1,36 @@
+﻿using SampleApp.FreshMVVM.Droid.CustomRendrers;
+using SampleApp.FreshMVVM.Interfaces;
+using System.Globalization;
+using System.Threading;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AppLocale))]
+namespace SampleApp.FreshMVVM.Droid.CustomRendrers
+{
+    public class AppLocale : IAppLocale
+    {
+        public void SetLocale(CultureInfo cultureInfo)
+        {
+            Thread.CurrentThread.CurrentUICulture = cultureInfo;
+        }
+
+        public CultureInfo GetCurrentCultureInfo()
+        {
+            var netLanguage = "en";
+            var androidLocale = Java.Util.Locale.Default;
+            netLanguage = androidLocale.ToString().Replace("_", "-");
+
+            System.Globalization.CultureInfo ci = null;
+            try
+            {
+                ci = new System.Globalization.CultureInfo(netLanguage);
+            }
+            catch (CultureNotFoundException ex)
+            {
+                ci = new System.Globalization.CultureInfo("en");
+            }
+
+            return ci;
+        }
+    }
+}
