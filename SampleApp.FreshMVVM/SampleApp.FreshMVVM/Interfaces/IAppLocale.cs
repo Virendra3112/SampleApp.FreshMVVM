b08c2d87d6e9306adc9db068efaa20ed53e12c15
Virendra <virendra.thakur31@gmail.com>
2021-04-04 20:51:45 +0530

    IAppLocale added.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Interfaces/IAppLocale.cs
@@ -0,0 +1,11 @@
+﻿using System.Globalization;
+
+namespace SampleApp.FreshMVVM.Interfaces
+{
+    public interface IAppLocale
+    {
+        CultureInfo GetCurrentCultureInfo();
+
+        void SetLocale(CultureInfo cultureInfo);
+    }
+}
