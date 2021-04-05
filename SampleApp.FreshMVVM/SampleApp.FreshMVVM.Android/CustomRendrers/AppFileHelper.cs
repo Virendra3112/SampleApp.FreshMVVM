61ac0aa7393ee5866de19cc0aeaf7ac5a45f25f1
Virendra <virendra.thakur31@gmail.com>
2021-04-05 11:30:24 +0530

    AppFileHelper added for android.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM.Android/CustomRendrers/AppFileHelper.cs
@@ -0,0 +1,21 @@
+﻿using SampleApp.FreshMVVM.Droid.CustomRendrers;
+using SampleApp.FreshMVVM.Interfaces;
+using System;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AppFileHelper))]
+namespace SampleApp.FreshMVVM.Droid.CustomRendrers
+{
+    public class AppFileHelper : IAppFileHelper
+    {
+        public bool? IsPreferredLanguageArabic()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveUserLanguagePreference(bool status)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
