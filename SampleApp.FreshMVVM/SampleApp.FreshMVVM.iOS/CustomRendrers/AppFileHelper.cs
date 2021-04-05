0c9e28ecb92822025cb8373e2d36120320ab03d4
Virendra <virendra.thakur31@gmail.com>
2021-04-05 11:30:58 +0530

    AppFileHelper added for iOS.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM.iOS/CustomRendrers/AppFileHelper.cs
@@ -0,0 +1,21 @@
+﻿using SampleApp.FreshMVVM.Interfaces;
+using SampleApp.FreshMVVM.iOS.CustomRendrers;
+using System;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(AppLocale))]
+namespace SampleApp.FreshMVVM.iOS.CustomRendrers
+{
+    public class AppFileHelper : IAppFileHelper
+    {
+        public bool? IsPreferredLanguageArabic()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveUserLanguagePreference(bool status)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
