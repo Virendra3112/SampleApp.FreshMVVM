4bfd2e8f374326e8c9779e5ad16b31cd4911cee2
Virendra <virendra.thakur31@gmail.com>
2021-04-06 10:46:21 +0530

    Language Enum added.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Models/Enum/LanguageEnum.cs
@@ -0,0 +1,17 @@
+﻿namespace SampleApp.FreshMVVM.Models.Enum
+{
+    public static class LanguageEnum
+    {
+        public enum LanguageCode
+        {
+            Arabic = 0,
+            English = 1
+        }
+
+        public enum LanguageShortCode
+        {
+            ar = 0,
+            en = 1
+        }
+    }
+}
