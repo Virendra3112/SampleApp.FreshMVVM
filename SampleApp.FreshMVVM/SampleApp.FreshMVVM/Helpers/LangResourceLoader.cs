03ce4c8967aee512ee1fc57040d2d3108295753a
Virendra <virendra.thakur31@gmail.com>
2021-04-07 11:30:29 +0530

    LangResourceLoader updated.

--- SampleApp.FreshMVVM/SampleApp.FreshMVVM/Helpers/LangResourceLoader.cs
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Helpers/LangResourceLoader.cs
@@ -1,10 +1,70 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using SampleApp.FreshMVVM.Resources;
+using System.ComponentModel;
+using System.Globalization;
+using System.Resources;
+using Xamarin.Forms;
+using static SampleApp.FreshMVVM.Models.Enum.LanguageEnum;
 
 namespace SampleApp.FreshMVVM.Helpers
 {
-    public class LangResourceLoader
+    public class LangResourceLoader : INotifyPropertyChanged
     {
+        private readonly ResourceManager manager = AppResources.ResourceManager;
+
+        static LangResourceLoader instance;
+        private LangResourceLoader()
+        {
+        }
+
+        public static void NullifySingleton()
+        {
+            instance = null;
+        }
+
+        public static LangResourceLoader Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new LangResourceLoader();
+                return instance;
+            }
+        }
+
+        public string GetString(string resourceName)
+        {
+            string stringRes = this.manager.GetString(resourceName);
+            return stringRes;
+        }
+
+        public string this[string key] => this.GetString(key);
+
+        public void SetCultureInfo(CultureInfo cultureInfo)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+        }
+
+        public void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(null));
+        }
+
+        public LanguageCode CurrentLanguageAbbr
+        {
+            get
+            {
+                if (Application.Current.Properties["Lang"] == null)
+                    return LanguageCode.English;
+
+                if (Application.Current.Properties["Lang"].ToString() == LanguageShortCode.ar.ToString())
+                {
+                    return LanguageCode.Arabic;
+                }
+
+                return LanguageCode.English;
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
