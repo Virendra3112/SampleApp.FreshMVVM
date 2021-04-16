2a76e559e9e01020c85f09aeb8dbb498b2b2ea1b
Virendra <virendra.thakur31@gmail.com>
2021-04-16 21:52:46 +0530

    ImageOperations added for android.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM.Android/CustomRendrers/ImageOperations.cs
@@ -0,0 +1,34 @@
+﻿using Android.Graphics;
+using SampleApp.FreshMVVM.Droid.CustomRendrers;
+using SampleApp.FreshMVVM.Interfaces;
+using System;
+using System.IO;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(ImageOperations))]
+namespace SampleApp.FreshMVVM.Droid.CustomRendrers
+{
+    public class ImageOperations : IImageOperations
+    {
+        public byte[] CompressImage(byte[] imageData, float width, float height)
+        {
+
+            var originalImage = BitmapFactory.DecodeByteArray(imageData, 0, imageData.Length);
+            var resizedImage = Bitmap.CreateScaledBitmap(originalImage, (int)width, (int)height, false);
+
+            using (MemoryStream memoryStream = new MemoryStream())
+            {
+                //resizedImage.SetPixels()
+                resizedImage.Compress(Bitmap.CompressFormat.Jpeg, 80, memoryStream);
+                return memoryStream.ToArray();
+            }
+
+
+
+
+
+        }
+
+
+    }
+}
