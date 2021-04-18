b17a5ab0ea3f0e1dcce2686529fb562fc4953191
Virendra <virendra.thakur31@gmail.com>
2021-04-18 14:44:31 +0530

    ImageOperations updated for iOS.

--- SampleApp.FreshMVVM/SampleApp.FreshMVVM.iOS/CustomRendrers/ImageOperations.cs
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM.iOS/CustomRendrers/ImageOperations.cs
@@ -1,6 +1,9 @@
-﻿using SampleApp.FreshMVVM.Interfaces;
+﻿using CoreGraphics;
+using SampleApp.FreshMVVM.Interfaces;
 using SampleApp.FreshMVVM.iOS.CustomRendrers;
 using System;
+using System.Drawing;
+using UIKit;
 using Xamarin.Forms;
 
 [assembly: Dependency(typeof(ImageOperations))]
@@ -10,7 +13,41 @@ namespace SampleApp.FreshMVVM.iOS.CustomRendrers
     {
         public byte[] CompressImage(byte[] imageData, float width, float height)
         {
-            throw new NotImplementedException();
+            UIImage sourceImage = ConvertImage(imageData);
+
+            UIImageOrientation orientation = sourceImage.Orientation;
+
+            using (CGBitmapContext cGBitmapContext = new CGBitmapContext(IntPtr.Zero, (int)width, (int)height, 8,
+              4 * (int)width, CGColorSpace.CreateDeviceRGB(), CGImageAlphaInfo.PremultipliedFirst))
+            {
+                RectangleF imageReact = new RectangleF(0, 0, width, height);
+
+                cGBitmapContext.DrawImage(imageReact, sourceImage.CGImage);
+
+                UIKit.UIImage resizedImage = UIKit.UIImage.FromImage(cGBitmapContext.ToImage(), 0, orientation);
+
+                return resizedImage.AsJPEG().ToArray();
+
+            }
+
+        }
+
+        private static UIKit.UIImage ConvertImage(byte[] ImageData)
+        {
+            try
+            {
+                if (ImageData == null)
+                    return null;
+
+                UIKit.UIImage uiImage = new UIImage(Foundation.NSData.FromArray(ImageData));
+
+                return uiImage;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+
         }
 
         public byte[] ConvertToJpg(string path)
