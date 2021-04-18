56db53b7c41c67102da86af522034af1294c4316
Virendra <virendra.thakur31@gmail.com>
2021-04-18 14:27:18 +0530

    ImageOperations added for iOS.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM.iOS/CustomRendrers/ImageOperations.cs
@@ -0,0 +1,21 @@
+﻿using SampleApp.FreshMVVM.Interfaces;
+using SampleApp.FreshMVVM.iOS.CustomRendrers;
+using System;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(ImageOperations))]
+namespace SampleApp.FreshMVVM.iOS.CustomRendrers
+{
+    public class ImageOperations : IImageOperations
+    {
+        public byte[] CompressImage(byte[] imageData, float width, float height)
+        {
+            throw new NotImplementedException();
+        }
+
+        public byte[] ConvertToJpg(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
