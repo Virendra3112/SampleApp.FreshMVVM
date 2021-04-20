5bcafbe2f6af443334ef0d312ef96ed82a78c941
Virendra <virendra.thakur31@gmail.com>
2021-04-20 21:37:43 +0530

    Value Formatter added.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Helpers/CustomDataSetValueFormatter.cs
@@ -0,0 +1,12 @@
+﻿using UltimateXF.Widget.Charts.Models.Formatters;
+
+namespace SampleApp.FreshMVVM.Helpers
+{
+    public class CustomDataSetValueFormatter : IDataSetValueFormatter
+    {
+        public string GetFormattedValue(float value, int dataSetIndex)
+        {
+            return value.ToString("N2") + "$";
+        }
+    }
+}
