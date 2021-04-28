8245a320f3ef2c0a8a07215ad4fbfa2bf37e1c54
Virendra <virendra.thakur31@gmail.com>
2021-04-28 12:36:22 +0530

    SampleVideoPlayerPageRendrer added for iOS.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM.iOS/CustomRendrers/SampleVideoPlayerPageRendrer.cs
@@ -0,0 +1,19 @@
+﻿using Foundation;
+using SampleApp.FreshMVVM.iOS.CustomRendrers;
+using SampleApp.FreshMVVM.Pages;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(SampleVideoPlayerPage), typeof(SampleVideoPlayerPageRendrer))]
+namespace SampleApp.FreshMVVM.iOS.CustomRendrers
+{
+    public class SampleVideoPlayerPageRendrer : PageRenderer
+    {
+        public override void ViewWillDisappear(bool animated)
+        {
+            base.ViewWillDisappear(animated);
+            UIDevice.CurrentDevice.SetValueForKey(NSNumber.FromNInt((int)(UIInterfaceOrientation.Portrait)), new NSString("orientation"));
+        }
+    }
+}
