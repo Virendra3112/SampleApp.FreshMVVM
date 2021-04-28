b1176afe840df7adadab46e633cef3921219c9db
Virendra <virendra.thakur31@gmail.com>
2021-04-28 13:03:42 +0530

    SALoginPage added.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Pages/SALoginPage.xaml.cs
@@ -0,0 +1,20 @@
+﻿
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SampleApp.FreshMVVM.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SALoginPage : ContentPage
+    {
+        public SALoginPage()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+        }
+    }
+}
