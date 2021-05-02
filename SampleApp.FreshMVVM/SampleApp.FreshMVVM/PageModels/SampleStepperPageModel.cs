2234ae4186d40234ff791fc8efd3ad3fc9b415f1
Virendra <virendra.thakur31@gmail.com>
2021-05-02 22:52:05 +0530

    SampleStepperPageModel added.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/PageModels/SampleStepperPageModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace SampleApp.FreshMVVM.PageModels
+{
+    public class SampleStepperPageModel : BasePageModel
+    {
+
+        public SampleStepperPageModel()
+        {
+
+        }
+
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+        }
+
+        protected override void ViewIsAppearing(object sender, EventArgs e)
+        {
+            base.ViewIsAppearing(sender, e);
+        }
+    }
+}
