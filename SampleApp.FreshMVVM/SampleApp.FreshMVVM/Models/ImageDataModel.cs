45ada5bea50727f8e213c57e16c5ceb0a21e60fd
Virendra <virendra.thakur31@gmail.com>
2021-05-04 19:39:05 +0530

    ImageDataModel added.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Models/ImageDataModel.cs
@@ -0,0 +1,9 @@
+﻿namespace SampleApp.FreshMVVM.Models
+{
+    public class ImageDataModel
+    {
+        public double Height { get; set; }
+        public double Width { get; set; }
+        public double DPI { get; set; }
+    }
+}
