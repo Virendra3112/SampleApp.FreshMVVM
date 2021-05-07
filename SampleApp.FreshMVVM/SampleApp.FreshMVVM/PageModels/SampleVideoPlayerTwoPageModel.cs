d2e18cb645a8f53591fbb0cfdc2f03bd9605bf2a
Virendra <virendra.thakur31@gmail.com>
2021-05-07 21:41:19 +0530

    SampleVideoPageTwo PageModel updated.

--- SampleApp.FreshMVVM/SampleApp.FreshMVVM/PageModels/SampleVideoPlayerTwoPageModel.cs
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/PageModels/SampleVideoPlayerTwoPageModel.cs
@@ -60,12 +60,12 @@ namespace SampleApp.FreshMVVM.PageModels
                 MediaPlayer.Play();
 
                 // subscribe to libvlc playback events
-                MediaPlayer.TimeChanged += TimeChanged;
-                MediaPlayer.PositionChanged += PositionChanged;
-                MediaPlayer.LengthChanged += LengthChanged;
-                MediaPlayer.EndReached += EndReached;
-                MediaPlayer.Playing += Playing;
-                MediaPlayer.Paused += Paused;
+                //MediaPlayer.TimeChanged += TimeChanged;
+                //MediaPlayer.PositionChanged += PositionChanged;
+                //MediaPlayer.LengthChanged += LengthChanged;
+                //MediaPlayer.EndReached += EndReached;
+                //MediaPlayer.Playing += Playing;
+                //MediaPlayer.Paused += Paused;
 
 
             }
@@ -75,29 +75,8 @@ namespace SampleApp.FreshMVVM.PageModels
             }
         }
 
-        private void PositionChanged(object sender, MediaPlayerPositionChangedEventArgs e)
-        {
-        }
-
-        private void LengthChanged(object sender, MediaPlayerLengthChangedEventArgs e)
-        {
-        }
-
-        private void EndReached(object sender, EventArgs e)
-        {
-        }
-
-        private void Playing(object sender, EventArgs e)
-        {
-        }
-
-        private void Paused(object sender, EventArgs e)
-        {
-        }
-
-        private void TimeChanged(object sender, MediaPlayerTimeChangedEventArgs e)
-        {
-        }
+      
+   
 
         public override void Init(object initData)
         {
@@ -113,8 +92,6 @@ namespace SampleApp.FreshMVVM.PageModels
 
             try
             {
-
-
                 //MediaPlayer.Play();
             }
             catch (Exception)
@@ -123,6 +100,104 @@ namespace SampleApp.FreshMVVM.PageModels
             }
         }
 
+
+        string _message = string.Empty;
+
+        public string Message
+        {
+            get { return _message; }
+            set { SetPropertyValue(ref _message, value); }
+        }
+
+
+        long _finalTime;
+        bool _timeChanged;
+
+        int _finalVolume;
+        bool _volumeChanged;
+
+        string FormatSeekingMessage(long timeDiff, long finalTime, Direction direction)
+        {
+            var timeDiffTimeSpan = TimeSpan.FromMilliseconds((double)new decimal(timeDiff));
+            var finalTimeSpan = TimeSpan.FromMilliseconds((double)new decimal(finalTime));
+            var diffSign = direction == Direction.Right ? "+" : "-";
+            return $"Seeking ({direction} swipe): {diffSign}{timeDiffTimeSpan.Minutes}:{Math.Abs(timeDiffTimeSpan.Seconds)} ({finalTimeSpan.Minutes}:{Math.Abs(finalTimeSpan.Seconds)})";
+        }
+
+        string FormatVolumeMessage(int volume, Direction direction) => $"Volume ({direction} swipe): {volume}%";
+
+        bool WillOverflow => _finalTime > TimeSpan.MaxValue.TotalMilliseconds || _finalTime < TimeSpan.MinValue.TotalMilliseconds;
+
+        int VolumeRangeCheck(int volume)
+        {
+            if (volume < 0)
+                volume = 0;
+            else if (volume > 200)
+                volume = 200;
+            return volume;
+        }
+
+        public void OnGesture(PanUpdatedEventArgs e)
+        {
+            switch (e.StatusType)
+            {
+                case GestureStatus.Running:
+                    if (e.TotalX < 0 && Math.Abs(e.TotalX) > Math.Abs(e.TotalY))
+                    {
+                        var timeDiff = Convert.ToInt64(e.TotalX * 1000);
+                        _finalTime = MediaPlayer.Time + timeDiff;
+
+                        if (WillOverflow)
+                            break;
+
+                        Message = FormatSeekingMessage(timeDiff, _finalTime, Direction.Left);
+                        _timeChanged = true;
+                    }
+                    else if (e.TotalX > 0 && Math.Abs(e.TotalX) > Math.Abs(e.TotalY))
+                    {
+                        var timeDiff = Convert.ToInt64(e.TotalX * 1000);
+                        _finalTime = MediaPlayer.Time + timeDiff;
+
+                        if (WillOverflow)
+                            break;
+
+                        Message = FormatSeekingMessage(timeDiff, _finalTime, Direction.Right);
+                        _timeChanged = true;
+                    }
+                    else if (e.TotalY < 0 && Math.Abs(e.TotalY) > Math.Abs(e.TotalX))
+                    {
+                        var volume = (int)(MediaPlayer.Volume + e.TotalY * -1);
+                        _finalVolume = VolumeRangeCheck(volume);
+
+                        Message = FormatVolumeMessage(_finalVolume, Direction.Top);
+                        _volumeChanged = true;
+                    }
+                    else if (e.TotalY > 0 && e.TotalY > Math.Abs(e.TotalX))
+                    {
+                        var volume = (int)(MediaPlayer.Volume + e.TotalY * -1);
+                        _finalVolume = VolumeRangeCheck(volume);
+
+                        Message = FormatVolumeMessage(_finalVolume, Direction.Bottom);
+                        _volumeChanged = true;
+                    }
+                    break;
+                case GestureStatus.Started:
+                case GestureStatus.Canceled:
+                    Message = string.Empty;
+                    break;
+                case GestureStatus.Completed:
+                    if (_timeChanged)
+                        MediaPlayer.Time = _finalTime;
+                    if (_volumeChanged && MediaPlayer.Volume != _finalVolume)
+                        MediaPlayer.Volume = _finalVolume;
+
+                    Message = string.Empty;
+                    _timeChanged = false;
+                    _volumeChanged = false;
+                    break;
+            }
+
+        }
 
     }
 
