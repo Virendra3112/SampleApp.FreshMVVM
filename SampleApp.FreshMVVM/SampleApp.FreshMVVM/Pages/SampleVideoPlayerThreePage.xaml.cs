b967eed4897db054b7832172984f8973a8d50f43
Virendra <virendra.thakur31@gmail.com>
2021-05-07 21:47:22 +0530

    SampleVideoPlayerThreePage.xaml added.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Pages/SampleVideoPlayerThreePage.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace SampleApp.FreshMVVM.Pages
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SampleVideoPlayerThreePage : ContentPage
+    {
+        public SampleVideoPlayerThreePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
