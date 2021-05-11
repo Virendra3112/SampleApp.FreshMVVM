fbaca9383f1e2a24d1ffcdf399c925daf4d88e07
Virendra <virendra.thakur31@gmail.com>
2021-05-11 11:12:16 +0530

    MyCustomDialogFragment updated.

--- SampleApp.FreshMVVM/SampleApp.FreshMVVM.Android/MyCustomDialogFragment.cs
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM.Android/MyCustomDialogFragment.cs
@@ -1,17 +1,18 @@
-﻿using Android.App;
-using Android.Content;
+﻿using Android.Graphics;
+using Android.Graphics.Drawables;
 using Android.OS;
-using Android.Runtime;
 using Android.Views;
-using Android.Widget;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using AndroidX.Biometric;
 
 namespace SampleApp.FreshMVVM.Droid
 {
-    class MyCustomDialogFragment
+    public class MyCustomDialogFragment : FingerprintDialogFragment
     {
+        public override View OnCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState)
+        {
+            var view = base.OnCreateView(inflater, container, savedInstanceState);
+            view.Background = new ColorDrawable(Color.Magenta);
+            return view;
+        }
     }
 }
