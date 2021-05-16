1b3701230181ad843c31b1ae1088804e04802592
Virendra <virendra.thakur31@gmail.com>
2021-05-16 23:22:17 +0530

    commit.

--- SampleApp.FreshMVVM/SampleApp.FreshMVVM/PageModels/SampleVideoPlayerPageModel.cs
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/PageModels/SampleVideoPlayerPageModel.cs
@@ -1,6 +1,24 @@
-﻿namespace SampleApp.FreshMVVM.PageModels
+﻿using System;
+
+namespace SampleApp.FreshMVVM.PageModels
 {
     public class SampleVideoPlayerPageModel : BasePageModel
     {
+        public SampleVideoPlayerPageModel()
+        {
+
+        }
+
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+        }
+
+        protected override void ViewIsAppearing(object sender, EventArgs e)
+        {
+            base.ViewIsAppearing(sender, e);
+
+            //http://tradeplusresources.com/demo/images/test.mp4
+        }
     }
 }
