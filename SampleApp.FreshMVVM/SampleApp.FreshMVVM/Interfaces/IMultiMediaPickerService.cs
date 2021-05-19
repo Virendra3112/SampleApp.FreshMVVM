275c046123c2a186a6cda25b11896326a3c66aaa
Virendra <virendra.thakur31@gmail.com>
2021-05-19 10:53:35 +0530

    IMultiMediaPickerService added.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Interfaces/IMultiMediaPickerService.cs
@@ -0,0 +1,16 @@
+﻿using SampleApp.FreshMVVM.Models;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SampleApp.FreshMVVM.Interfaces
+{
+    public interface IMultiMediaPickerService
+    {
+        event EventHandler<MediaFile> OnMediaPicked;
+        event EventHandler<IList<MediaFile>> OnMediaPickedCompleted;
+        Task<IList<MediaFile>> PickPhotosAsync();
+        Task<IList<MediaFile>> PickVideosAsync();
+        void Clean();
+    }
+}
