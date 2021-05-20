ce94739a9eb2a09d20060162ab4fc74438ae19ca
Virendra <virendra.thakur31@gmail.com>
2021-05-20 10:49:39 +0530

    MultiMediaPickerService and Image helper updated.

--- SampleApp.FreshMVVM/SampleApp.FreshMVVM.Android/CustomRendrers/ImageHelpers.cs
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM.Android/CustomRendrers/ImageHelpers.cs
@@ -1,17 +1,65 @@
-﻿using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using System;
+using System.IO;
+using Android.Graphics;
+using Android.Media;
 
 namespace SampleApp.FreshMVVM.Droid.CustomRendrers
 {
-    class ImageHelpers
+    public static class ImageHelpers
     {
+
+        public static byte[] RotateImage(string path, float scaleFactor, int quality = 90)
+        {
+            byte[] imageBytes;
+
+            var originalImage = BitmapFactory.DecodeFile(path);
+            var rotation = GetRotation(path);
+            var width = (originalImage.Width * scaleFactor);
+            var height = (originalImage.Height * scaleFactor);
+            var scaledImage = Bitmap.CreateScaledBitmap(originalImage, (int)width, (int)height, true);
+
+            Bitmap rotatedImage = scaledImage;
+            if (rotation != 0)
+            {
+                var matrix = new Matrix();
+                matrix.PostRotate(rotation);
+                rotatedImage = Bitmap.CreateBitmap(scaledImage, 0, 0, scaledImage.Width, scaledImage.Height, matrix, true);
+                scaledImage.Recycle();
+                scaledImage.Dispose();
+            }
+
+            using (var ms = new MemoryStream())
+            {
+                rotatedImage.Compress(Bitmap.CompressFormat.Jpeg, quality, ms);
+                imageBytes = ms.ToArray();
+            }
+
+
+            originalImage?.Dispose();
+            rotatedImage?.Dispose();
+            GC.Collect();
+
+            return imageBytes;
+        }
+
+        static int GetRotation(string filePath)
+        {
+            using (var ei = new ExifInterface(filePath))
+            {
+                var orientation = (Android.Media.Orientation)ei.GetAttributeInt(ExifInterface.TagOrientation, (int)Android.Media.Orientation.Normal);
+
+                switch (orientation)
+                {
+                    case Android.Media.Orientation.Rotate90:
+                        return 90;
+                    case Android.Media.Orientation.Rotate180:
+                        return 180;
+                    case Android.Media.Orientation.Rotate270:
+                        return 270;
+                    default:
+                        return 0;
+                }
+            }
+        }
     }
 }
