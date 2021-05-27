6e35eefbffcce7c83afa67b0c4d0ed5e32967073
Virendra <virendra.thakur31@gmail.com>
2021-05-27 12:14:45 +0530

    AppOperations added for Android.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM.Android/CustomRendrers/AppOperations.cs
@@ -0,0 +1,13 @@
+﻿using SampleApp.FreshMVVM.Interfaces;
+using System;
+
+namespace SampleApp.FreshMVVM.Droid.CustomRendrers
+{
+    public class AppOperations : IAppOperations
+    {
+        public string CheckAppVersion(string currentVersion)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
