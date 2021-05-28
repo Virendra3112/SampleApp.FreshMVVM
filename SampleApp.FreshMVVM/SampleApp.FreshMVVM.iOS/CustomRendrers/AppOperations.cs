2ac83b44f9b5bb167a30ee8d2165d16882f8dd5a
Virendra <virendra.thakur31@gmail.com>
2021-05-28 10:58:41 +0530

    AppOperations updated for iOS.

--- SampleApp.FreshMVVM/SampleApp.FreshMVVM.iOS/CustomRendrers/AppOperations.cs
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM.iOS/CustomRendrers/AppOperations.cs
@@ -1,14 +1,42 @@
-﻿using SampleApp.FreshMVVM.Interfaces;
+﻿using Foundation;
+using SampleApp.FreshMVVM.Interfaces;
 using System;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace SampleApp.FreshMVVM.iOS.CustomRendrers
 {
     public class AppOperations : IAppOperations
     {
-        public Task<string> CheckAppVersion(string currentVersion)
+        public string bundleIdentifier { get; set; }
+        public string bundleVersion { get; set; }
+        public string Version { get; set; }
+        public string Url { get; set; }
+
+        public AppOperations()
         {
-            throw new NotImplementedException();
+            bundleIdentifier = NSBundle.MainBundle.ObjectForInfoDictionary("CFBundleIdentifier").ToString();
+            bundleVersion = NSBundle.MainBundle.ObjectForInfoDictionary("CFBundleShortVersionString").ToString();
+        }
+        public async Task<string> CheckAppVersion(string currentVersion)
+        {
+            var countryCode = "us";//specify country code
+
+            try
+            {
+                using (var http = new HttpClient())
+                {
+                    var response = await http.GetAsync($"http://itunes.apple.com/{countryCode}/lookup?bundleId={bundleIdentifier}");
+                    var content = response.Content == null ? null : await response.Content.ReadAsStringAsync();
+                    var appLookup = System.Json.JsonValue.Parse(content);
+
+                    return appLookup["results"][0]["version"] + "" + appLookup["results"][0]["trackViewUrl"];                  
+                };
+            }
+            catch (Exception e)
+            {
+                return null;
+            }
         }
 
         public Task OpenAppInStore()
