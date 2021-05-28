0a7792abbb1c7332d365ed43a545462f715e46ca
Virendra <virendra.thakur31@gmail.com>
2021-05-28 10:39:06 +0530

    AppOperations added for iOS.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM.iOS/CustomRendrers/AppOperations.cs
@@ -0,0 +1,19 @@
+﻿using SampleApp.FreshMVVM.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace SampleApp.FreshMVVM.iOS.CustomRendrers
+{
+    public class AppOperations : IAppOperations
+    {
+        public Task<string> CheckAppVersion(string currentVersion)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task OpenAppInStore()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
