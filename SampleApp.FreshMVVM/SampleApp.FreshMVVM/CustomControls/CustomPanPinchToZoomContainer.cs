23aaf65e42820ce81797a49dd9be204fa5215e80
Virendra <virendra.thakur31@gmail.com>
2021-05-30 21:57:29 +0530

    CustomPanPinchToZoomContainer added.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/CustomControls/CustomPanPinchToZoomContainer.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleApp.FreshMVVM.CustomControls
+{
+    public class CustomPanPinchToZoomContainer
+    {
+    }
+}
