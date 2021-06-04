77406b4d6c0c157d2f635eabfd59f8355a1fba53
Virendra <virendra.thakur31@gmail.com>
2021-06-04 18:08:25 +0530

    MainActivity updated.

--- SampleApp.FreshMVVM/SampleApp.FreshMVVM.Android/MainActivity.cs
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM.Android/MainActivity.cs
@@ -12,6 +12,10 @@ using SampleApp.FreshMVVM.Interfaces;
 using Plugin.Permissions;
 using FFImageLoading.Forms.Platform;
 using FFImageLoading.Svg.Forms;
+using Firebase;
+using System;
+using Firebase.Iid;
+using Android.Gms.Common;
 //using LibVLCSharp.Forms.Shared;
 //using Plugin.MediaManager.Forms.Android;
 //using UltimateXF.Droid;
@@ -22,6 +26,10 @@ namespace SampleApp.FreshMVVM.Droid
     [Activity(Label = "SampleApp.FreshMVVM", Icon = "@mipmap/icon", Theme = "@style/MyTheme.Splash", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        static readonly string TAG = "MainActivity";
+
+        internal static readonly string CHANNEL_ID = "my_notification_channel";
+        internal static readonly int NOTIFICATION_ID = 100;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             try
@@ -52,6 +60,20 @@ namespace SampleApp.FreshMVVM.Droid
                 var ignore = typeof(SvgCachedImage);
 
                 //LibVLCSharpFormsRenderer.Init();
+
+                IsPlayServicesAvailable(); //You can use this method to check if play services are available.
+                CreateNotificationChannel();
+
+                try
+                {
+                    FirebaseApp.InitializeApp(this);
+                    Console.WriteLine(TAG, "InstanceID token: " + FirebaseInstanceId.Instance.Token);
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(TAG, "InstanceID token Error " + ex.Message);
+                }
 
 
                 //allowing the device to change the screen orientation based on the rotation 
@@ -90,6 +112,49 @@ namespace SampleApp.FreshMVVM.Droid
             base.OnActivityResult(requestCode, resultCode, data);
 
             MultiMediaPickerService.SharedInstance.OnActivityResult(requestCode, resultCode, data);
+        }
+
+        public bool IsPlayServicesAvailable()
+        {
+            int resultCode = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (resultCode != ConnectionResult.Success)
+            {
+                //if (GoogleApiAvailability.Instance.IsUserResolvableError(resultCode))
+                //    msgText.Text = GoogleApiAvailability.Instance.GetErrorString(resultCode);
+                //else
+                //{
+                //  msgText.Text = "This device is not supported";
+                Finish();
+                //  }
+                return false;
+            }
+            else
+            {
+                // msgText.Text = "Google Play Services is available.";
+                return true;
+            }
+        }
+
+        void CreateNotificationChannel()
+        {
+            if (Build.VERSION.SdkInt < BuildVersionCodes.O)
+            {
+                // Notification channels are new in API 26 (and not a part of the
+                // support library). There is no need to create a notification
+                // channel on older versions of Android.
+                return;
+            }
+
+            var channel = new NotificationChannel(CHANNEL_ID,
+                                                  "FCM Notifications",
+                                                  NotificationImportance.High)
+            {
+
+                Description = "Firebase Cloud Messages appear in this channel"
+            };
+
+            var notificationManager = (NotificationManager)GetSystemService(Android.Content.Context.NotificationService);
+            notificationManager.CreateNotificationChannel(channel);
         }
     }
 }
