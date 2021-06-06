4cafd0972826682d214cb1a6dfa1c24ac2d290f1
Virendra <virendra.thakur31@gmail.com>
2021-06-06 17:45:46 +0530

    IAppLogger added.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Interfaces/IAppLogger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace SampleApp.FreshMVVM.Interfaces
+{
+    public interface IAppLogger
+    {
+        Task SetupLogger(string fileName);
+        Task AddLog(string ClassName, string method, string parameters);
+        Task AddErrorLog(string ClassName, string method, Exception ex, string parameters);
+    }
+}
