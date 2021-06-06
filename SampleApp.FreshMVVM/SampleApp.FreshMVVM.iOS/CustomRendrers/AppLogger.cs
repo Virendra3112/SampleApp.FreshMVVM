53053e5dd5f2ffaa9d1eb768cb1334d7ff0540bf
Virendra <virendra.thakur31@gmail.com>
2021-06-06 17:47:11 +0530

    AppLogger added for iOS.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM.iOS/CustomRendrers/AppLogger.cs
@@ -0,0 +1,24 @@
+﻿using SampleApp.FreshMVVM.Interfaces;
+using System;
+using System.Threading.Tasks;
+
+namespace SampleApp.FreshMVVM.iOS.CustomRendrers
+{
+    public class AppLogger : IAppLogger
+    {
+        public Task AddErrorLog(string ClassName, string method, Exception ex, string parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task AddLog(string ClassName, string method, string parameters)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task SetupLogger(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
