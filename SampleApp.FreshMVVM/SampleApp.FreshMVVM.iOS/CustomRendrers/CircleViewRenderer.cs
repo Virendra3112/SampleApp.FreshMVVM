eb885b0108a537e57de0a4d52643bd355d7deb69
Virendra <virendra.thakur31@gmail.com>
2021-06-09 22:22:43 +0530

    CircleView and BadgeView added.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM.iOS/CustomRendrers/CircleViewRenderer.cs
@@ -0,0 +1,23 @@
+﻿using SampleApp.FreshMVVM.CustomControls;
+using SampleApp.FreshMVVM.iOS.CustomRendrers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(CircleView), typeof(CircleViewRenderer))]
+namespace SampleApp.FreshMVVM.iOS.CustomRendrers
+{
+    public class CircleViewRenderer : BoxRenderer
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<BoxView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Element == null)
+                return;
+
+            Layer.MasksToBounds = true;
+            Layer.CornerRadius = (float)((CircleView)Element).CornerRadius / 2.0f;
+        }
+
+    }
+}
---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/CustomControls/CircleView.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SampleApp.FreshMVVM.CustomControls
+{
+    public partial class CircleView : BoxView
+    {
+        public static readonly BindableProperty CornerRadiusProperty = BindableProperty.Create(nameof(CornerRadius), typeof(double), typeof(CircleView), 0.0);
+
+        public double CornerRadius
+        {
+            get { return (double)GetValue(CornerRadiusProperty); }
+            set { SetValue(CornerRadiusProperty, value); }
+        }
+
+        public CircleView()
+        {
+            //InitializeComponent();
+        }
+    }
+}
