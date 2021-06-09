a76c6db0aeeaed0d91ed941b2a3fee3d74513938
Virendra <virendra.thakur31@gmail.com>
2021-06-09 18:03:06 +0530

    AppNotificationPageModel added.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/PageModels/AppNotificationPageModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SampleApp.FreshMVVM.PageModels
+{
+    public class AppNotificationPageModel : BasePageModel
+    {
+
+        public AppNotificationPageModel()
+        {
+
+        }
+
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+        }
+
+        protected override void ViewIsAppearing(object sender, EventArgs e)
+        {
+            base.ViewIsAppearing(sender, e);
+        }
+    }
+}
