cfdb83dce35ac424c3fbeeef3476bd1346b33298
Virendra <virendra.thakur31@gmail.com>
2021-06-10 11:05:58 +0530

    CircleViewRenderer updated for android.

--- SampleApp.FreshMVVM/SampleApp.FreshMVVM.Android/CustomRendrers/CircleViewRenderer.cs
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM.Android/CustomRendrers/CircleViewRenderer.cs
@@ -1,17 +1,60 @@
-﻿using Android.App;
-using Android.Content;
-using Android.OS;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+﻿using Android.Content;
+using Android.Graphics;
+using Android.Util;
+using SampleApp.FreshMVVM.CustomControls;
+using SampleApp.FreshMVVM.Droid.CustomRendrers;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
 
+[assembly: ExportRenderer(typeof(CircleView), typeof(CircleViewRenderer))]
 namespace SampleApp.FreshMVVM.Droid.CustomRendrers
 {
-    class CircleViewRenderer
+    public class CircleViewRenderer : BoxRenderer
     {
+        private float _cornerRadius;
+        private RectF _bounds;
+        private Path _path;
+
+
+        public CircleViewRenderer(Context context) : base(context)
+        {
+
+        }
+
+        protected override void OnElementChanged(ElementChangedEventArgs<BoxView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Element == null)
+            {
+                return;
+            }
+            var element = (CircleView)Element;
+
+            _cornerRadius = TypedValue.ApplyDimension(ComplexUnitType.Dip, (float)element.CornerRadius, Context.Resources.DisplayMetrics);
+
+        }
+
+        protected override void OnSizeChanged(int w, int h, int oldw, int oldh)
+        {
+            base.OnSizeChanged(w, h, oldw, oldh);
+            if (w != oldw && h != oldh)
+            {
+                _bounds = new RectF(0, 0, w, h);
+            }
+
+            _path = new Path();
+            _path.Reset();
+            _path.AddRoundRect(_bounds, _cornerRadius, _cornerRadius, Path.Direction.Cw);
+            _path.Close();
+        }
+
+        public override void Draw(Canvas canvas)
+        {
+            canvas.Save();
+            canvas.ClipPath(_path);
+            base.Draw(canvas);
+            canvas.Restore();
+        }
     }
 }
