df81a9c12a623e96a493d599f35794051f2d72bc
Virendra <virendra.thakur31@gmail.com>
2021-06-15 19:53:00 +0530

    HomePageModel updated.

--- SampleApp.FreshMVVM/SampleApp.FreshMVVM/PageModels/HomePageModel.cs
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/PageModels/HomePageModel.cs
@@ -1,8 +1,10 @@
 ﻿using SampleApp.FreshMVVM.Helpers;
+using SampleApp.FreshMVVM.Interfaces;
 using SampleApp.FreshMVVM.Models;
 using System;
 using System.Collections.ObjectModel;
 using Xamarin.Essentials;
+using Xamarin.Forms;
 
 namespace SampleApp.FreshMVVM.PageModels
 {
@@ -88,108 +90,115 @@ namespace SampleApp.FreshMVVM.PageModels
 
         private void NavigateToPage(MenuItems selectedItem)
         {
-            switch (selectedItem.Name)
+            try
             {
-                case "Home":
-                    AppHelper.MenuIsPresented = false;
-                    break;
-
-                case "Custom Loader":
-                    CoreMethods.PushPageModel<CustomLoaderSamplePageModel>();
-                    AppHelper.MenuIsPresented = false;
-
-                    break;
-
-                case "Custom Tabs":
-                    CoreMethods.PushPageModel<CustomScrollableTabsPageModel>();
-                    AppHelper.MenuIsPresented = false;
-
-                    break;
-
-                case "Image Compression":
-                    CoreMethods.PushPageModel<CustomImageCompressionPageModel>();
-                    AppHelper.MenuIsPresented = false;
-
-                    break;
-
-                case "Line Chart":
-                    CoreMethods.PushPageModel<CustomLineChartPageModel>();
-                    AppHelper.MenuIsPresented = false;
-
-                    break;
-
-                case "Ultimate XF Line Chart":
-                    CoreMethods.PushPageModel<SampleChartPageModel>();
-                    AppHelper.MenuIsPresented = false;
-
-                    break;
-
-                case "Video Player":
-                    CoreMethods.PushPageModel<SampleVideoPlayerPageModel>();
-                    AppHelper.MenuIsPresented = false;
-
-                    break;
-
-                case "Sample App Wireframe":
-                    CoreMethods.PushPageModel<SALoginPageModel>();
-                    AppHelper.MenuIsPresented = false;
-
-                    break;
-
-                case "video player2":
-                    //CoreMethods.PushPageModel<SampleVideoPlayerTwoPageModel>();
-                    AppHelper.MenuIsPresented = false;
-
-                    break;
-
-                case "Sample Stepper":
-                    CoreMethods.PushPageModel<SampleStepperPageModel>();
-                    AppHelper.MenuIsPresented = false;
-
-                    break;
-
-                case "Video Player3":
-                    CoreMethods.PushPageModel<SampleVideoPlayerThreePageModel>();
-                    AppHelper.MenuIsPresented = false;
-
-                    break;
-
-                case "Fingerprint":
-                    CoreMethods.PushPageModel<CustomFingerprintPageModel>();
-                    AppHelper.MenuIsPresented = false;
-
-                    break;
-
-                case "ImageEdit":
-                    CoreMethods.PushPageModel<SampleImageEditPageModel>();
-                    AppHelper.MenuIsPresented = false;
-
-                    break;
-
-                case "PanPinchSVG":
-                    CoreMethods.PushPageModel<CustomPanPinchSamplePageModel>();
-                    AppHelper.MenuIsPresented = false;
-
-                    break;
-
-                case "CarouselWithSVG":
-                    CoreMethods.PushPageModel<CarouselWithSVGPageModel>();
-                    AppHelper.MenuIsPresented = false;
-
-                    break;
-
-                case "Notifications":
-                    CoreMethods.PushPageModel<AppNotificationPageModel>();
-                    AppHelper.MenuIsPresented = false;
-
-                    break;
-
-                case "Export Logs":
-                    {
+                switch (selectedItem.Name)
+                {
+                    case "Home":
                         AppHelper.MenuIsPresented = false;
-                        ExportLogs();
-                    }
-                    break;
+                        break;
+
+                    case "Custom Loader":
+                        CoreMethods.PushPageModel<CustomLoaderSamplePageModel>();
+                        AppHelper.MenuIsPresented = false;
+
+                        break;
+
+                    case "Custom Tabs":
+                        CoreMethods.PushPageModel<CustomScrollableTabsPageModel>();
+                        AppHelper.MenuIsPresented = false;
+
+                        break;
+
+                    case "Image Compression":
+                        CoreMethods.PushPageModel<CustomImageCompressionPageModel>();
+                        AppHelper.MenuIsPresented = false;
+
+                        break;
+
+                    case "Line Chart":
+                        CoreMethods.PushPageModel<CustomLineChartPageModel>();
+                        AppHelper.MenuIsPresented = false;
+
+                        break;
+
+                    case "Ultimate XF Line Chart":
+                        CoreMethods.PushPageModel<SampleChartPageModel>();
+                        AppHelper.MenuIsPresented = false;
+
+                        break;
+
+                    case "Video Player":
+                        CoreMethods.PushPageModel<SampleVideoPlayerPageModel>();
+                        AppHelper.MenuIsPresented = false;
+
+                        break;
+
+                    case "Sample App Wireframe":
+                        CoreMethods.PushPageModel<SALoginPageModel>();
+                        AppHelper.MenuIsPresented = false;
+
+                        break;
+
+                    case "video player2":
+                        //CoreMethods.PushPageModel<SampleVideoPlayerTwoPageModel>();
+                        AppHelper.MenuIsPresented = false;
+
+                        break;
+
+                    case "Sample Stepper":
+                        CoreMethods.PushPageModel<SampleStepperPageModel>();
+                        AppHelper.MenuIsPresented = false;
+
+                        break;
+
+                    case "Video Player3":
+                        CoreMethods.PushPageModel<SampleVideoPlayerThreePageModel>();
+                        AppHelper.MenuIsPresented = false;
+
+                        break;
+
+                    case "Fingerprint":
+                        CoreMethods.PushPageModel<CustomFingerprintPageModel>();
+                        AppHelper.MenuIsPresented = false;
+
+                        break;
+
+                    case "ImageEdit":
+                        CoreMethods.PushPageModel<SampleImageEditPageModel>();
+                        AppHelper.MenuIsPresented = false;
+
+                        break;
+
+                    case "PanPinchSVG":
+                        CoreMethods.PushPageModel<CustomPanPinchSamplePageModel>();
+                        AppHelper.MenuIsPresented = false;
+
+                        break;
+
+                    case "CarouselWithSVG":
+                        CoreMethods.PushPageModel<CarouselWithSVGPageModel>();
+                        AppHelper.MenuIsPresented = false;
+
+                        break;
+
+                    case "Notifications":
+                        CoreMethods.PushPageModel<AppNotificationPageModel>();
+                        AppHelper.MenuIsPresented = false;
+
+                        break;
+
+                    case "Export Logs":
+                        {
+                            AppHelper.MenuIsPresented = false;
+                            ExportLogs();
+                        }
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                DependencyService.Resolve<IAppLogger>().AddErrorLog(nameof(HomePageModel), nameof(NavigateToPage), ex, selectedItem.Name);
             }
         }
 
