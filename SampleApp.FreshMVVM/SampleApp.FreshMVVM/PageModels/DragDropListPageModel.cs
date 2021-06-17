1ac40c692d8d9e69f28d2d66ce7805e1bc594672
Virendra <virendra.thakur31@gmail.com>
2021-06-17 22:20:03 +0530

    DragDropListPageModel updated.

--- SampleApp.FreshMVVM/SampleApp.FreshMVVM/PageModels/DragDropListPageModel.cs
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/PageModels/DragDropListPageModel.cs
@@ -1,6 +1,24 @@
-﻿namespace SampleApp.FreshMVVM.PageModels
+﻿using System;
+
+namespace SampleApp.FreshMVVM.PageModels
 {
     public class DragDropListPageModel : BasePageModel
     {
+
+        public DragDropListPageModel()
+        {
+
+        }
+
+        public override void Init(object initData)
+        {
+            base.Init(initData);
+        }
+
+
+        protected override void ViewIsAppearing(object sender, EventArgs e)
+        {
+            base.ViewIsAppearing(sender, e);
+        }
     }
 }
