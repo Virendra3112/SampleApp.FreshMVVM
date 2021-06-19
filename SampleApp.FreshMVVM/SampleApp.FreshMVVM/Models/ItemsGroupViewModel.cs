a4f3f192d2e5b57cd925caba3a21c1768317740a
Virendra <virendra.thakur31@gmail.com>
2021-06-19 21:02:46 +0530

    ItemsGroupViewModel added.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Models/ItemsGroupViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace SampleApp.FreshMVVM.Models
+{
+    public class ItemsGroupViewModel : ObservableCollection<ItemViewModel>
+    {
+        public string Name { get; set; }
+
+        public ItemsGroupViewModel(string name, IEnumerable<ItemViewModel> items)
+            : base(items)
+        {
+            Name = name;
+        }
+    }
+}
