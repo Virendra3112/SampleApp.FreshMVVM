b887c83cf8b3ad963da1de93659d24286f0978b6
Virendra <virendra.thakur31@gmail.com>
2021-06-19 21:07:29 +0530

    DragDropListPageModel updated.

--- SampleApp.FreshMVVM/SampleApp.FreshMVVM/PageModels/DragDropListPageModel.cs
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/PageModels/DragDropListPageModel.cs
@@ -1,13 +1,54 @@
-﻿using System;
+﻿using SampleApp.FreshMVVM.Models;
+using System;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 
 namespace SampleApp.FreshMVVM.PageModels
 {
     public class DragDropListPageModel : BasePageModel
     {
+        private ObservableCollection<ItemViewModel> _items = new ObservableCollection<ItemViewModel>();
+        public ObservableCollection<ItemViewModel> Items
+        {
+            get { return _items; }
+            set { SetPropertyValue(ref _items, value); }
+        }
 
+        private ObservableCollection<ItemsGroupViewModel> _groupedItems = new ObservableCollection<ItemsGroupViewModel>();
+        public ObservableCollection<ItemsGroupViewModel> GroupedItems
+        {
+            get { return _groupedItems; }
+            set { SetPropertyValue(ref _groupedItems, value); }
+        }
+
+        public ICommand StateRefresh { get; }
+
+        public ICommand StateReset { get; }
+
+        public ICommand StateTest { get; }
+
+        public ICommand ItemDragged { get; }
+
+        public ICommand ItemDraggedOver { get; }
+
+        public ICommand ItemDragLeave { get; }
+
+        public ICommand ItemDropped { get; }
         public DragDropListPageModel()
         {
-
+            StateRefresh = new Command(OnStateRefresh);
+            StateReset = new Command(OnStateReset);
+            StateTest = new Command(OnStateTest);
+            ItemDragged = new Command<ItemViewModel>(OnItemDragged);
+            ItemDraggedOver = new Command<ItemViewModel>(OnItemDraggedOver);
+            ItemDragLeave = new Command<ItemViewModel>(OnItemDragLeave);
+            ItemDropped = new Command<ItemViewModel>(i => OnItemDropped(i));
+            ResetItemsState();
         }
 
         public override void Init(object initData)
@@ -19,6 +60,106 @@ namespace SampleApp.FreshMVVM.PageModels
         protected override void ViewIsAppearing(object sender, EventArgs e)
         {
             base.ViewIsAppearing(sender, e);
+        }
+
+        private void OnStateRefresh()
+        {
+            Debug.WriteLine($"OnStateRefresh");
+            //OnPropertyChanged(nameof(Items));
+            PrintItemsState();
+        }
+
+        private void OnStateReset()
+        {
+            Debug.WriteLine($"OnStateReset");
+            ResetItemsState();
+            PrintItemsState();
+        }
+
+        private void OnStateTest()
+        {
+            Items.RemoveAt(4);
+            Items.Insert(0, new ItemViewModel { Title = "Item new 1" });
+            PrintItemsState();
+        }
+
+        private void OnItemDragged(ItemViewModel item)
+        {
+            Debug.WriteLine($"OnItemDragged: {item?.Title}");
+            Items.ForEach(i => i.IsBeingDragged = item == i);
+        }
+
+        private void OnItemDraggedOver(ItemViewModel item)
+        {
+            Debug.WriteLine($"OnItemDraggedOver: {item?.Title}");
+            var itemBeingDragged = _items.FirstOrDefault(i => i.IsBeingDragged);
+            Items.ForEach(i => i.IsBeingDraggedOver = item == i && item != itemBeingDragged);
+        }
+
+        private void OnItemDragLeave(ItemViewModel item)
+        {
+            Debug.WriteLine($"OnItemDragLeave: {item?.Title}");
+            Items.ForEach(i => i.IsBeingDraggedOver = false);
+        }
+
+        private async Task OnItemDropped(ItemViewModel item)
+        {
+            var itemToMove = _items.First(i => i.IsBeingDragged);
+            var itemToInsertBefore = item;
+
+            if (itemToMove == null || itemToInsertBefore == null || itemToMove == itemToInsertBefore)
+                return;
+
+            var categoryToMoveFrom = GroupedItems.First(g => g.Contains(itemToMove));
+            categoryToMoveFrom.Remove(itemToMove);
+
+            // Wait for remove animation to be completed
+            // https://github.com/xamarin/Xamarin.Forms/issues/13791
+            // await Task.Delay(1000);
+
+            var categoryToMoveTo = GroupedItems.First(g => g.Contains(itemToInsertBefore));
+            var insertAtIndex = categoryToMoveTo.IndexOf(itemToInsertBefore);
+            itemToMove.Category = categoryToMoveFrom.Name;
+            categoryToMoveTo.Insert(insertAtIndex, itemToMove);
+            itemToMove.IsBeingDragged = false;
+            itemToInsertBefore.IsBeingDraggedOver = false;
+            Debug.WriteLine($"OnItemDropped: [{itemToMove?.Title}] => [{itemToInsertBefore?.Title}], target index = [{insertAtIndex}]");
+
+            PrintItemsState();
+        }
+
+        private void ResetItemsState()
+        {
+            Items.Clear();
+            Items.Add(new ItemViewModel { Category = "Category 1", Title = "Item 1" });
+            Items.Add(new ItemViewModel { Category = "Category 1", Title = "Item 2" });
+            Items.Add(new ItemViewModel { Category = "Category 2", Title = "Item 3" });
+            Items.Add(new ItemViewModel { Category = "Category 2", Title = "Item 4" });
+            Items.Add(new ItemViewModel { Category = "Category 2", Title = "Item 5" });
+            Items.Add(new ItemViewModel { Category = "Category 2", Title = "Item 6" });
+            Items.Add(new ItemViewModel { Category = "Category 3", Title = "Item 7" });
+            Items.Add(new ItemViewModel { Category = "Category 3", Title = "Item 8" });
+            Items.Add(new ItemViewModel { Category = "Category 3", Title = "Item 9" });
+            Items.Add(new ItemViewModel { Category = "Category 3", Title = "Item 10" });
+            Items.Add(new ItemViewModel { Category = "Category 3", Title = "Item 11" });
+            Items.Add(new ItemViewModel { Category = "Category 3", Title = "Item 12" });
+            Items.Add(new ItemViewModel { Category = "Category 3", Title = "Item 13" });
+            Items.Add(new ItemViewModel { Category = "Category 3", Title = "Item 14" });
+            Items.Add(new ItemViewModel { Category = "Category 3", Title = "Item 15" });
+
+            GroupedItems = new ObservableCollection<ItemsGroupViewModel>(Items
+                .GroupBy(i => i.Category)
+                .Select(g => new ItemsGroupViewModel(g.Key, g)).ToList());
+            //.ToObservableCollection();
+        }
+
+        private void PrintItemsState()
+        {
+            Debug.WriteLine($"Items {Items.Count}, state:");
+            for (int i = 0; i < Items.Count; i++)
+            {
+                Debug.WriteLine($"\t{i}: Group: {Items[i].Category} | Item: {Items[i].Title}");
+            }
         }
     }
 }
