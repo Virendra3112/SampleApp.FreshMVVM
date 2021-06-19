764fe8e623819624a6e8496c71308e208156e8dc
Virendra <virendra.thakur31@gmail.com>
2021-06-19 21:01:09 +0530

    ItemViewModel added.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Models/ItemViewModel.cs
@@ -0,0 +1,24 @@
+﻿using Xamarin.CommunityToolkit.ObjectModel;
+
+namespace SampleApp.FreshMVVM.Models
+{
+    public class ItemViewModel : ObservableObject
+    {
+        public string Category { get; set; }
+        public string Title { get; set; }
+
+        private bool _isBeingDragged;
+        public bool IsBeingDragged
+        {
+            get { return _isBeingDragged; }
+            set { SetProperty(ref _isBeingDragged, value); }
+        }
+
+        private bool _isBeingDraggedOver;
+        public bool IsBeingDraggedOver
+        {
+            get { return _isBeingDraggedOver; }
+            set { SetProperty(ref _isBeingDraggedOver, value); }
+        }
+    }
+}
