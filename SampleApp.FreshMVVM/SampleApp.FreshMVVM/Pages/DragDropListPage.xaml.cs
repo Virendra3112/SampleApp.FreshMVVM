4d5b50f41e41c1e66d5a9463e97c2ae57e27719d
Virendra <virendra.thakur31@gmail.com>
2021-06-20 14:27:34 +0530

    DragDropListPage.xaml.cs updated.

--- SampleApp.FreshMVVM/SampleApp.FreshMVVM/Pages/DragDropListPage.xaml.cs
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Pages/DragDropListPage.xaml.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -15,6 +12,46 @@ namespace SampleApp.FreshMVVM.Pages
         public DragDropListPage()
         {
             InitializeComponent();
+        }
+
+        private void DragGestureRecognizer_DragStarting(Object sender, DragStartingEventArgs e)
+        {
+            var label = (Label)((Element)sender).Parent;
+            Debug.WriteLine($"DragGestureRecognizer_DragStarting [{label.Text}]");
+
+            e.Data.Properties["Label"] = label;
+
+            //e.Handled = true;
+        }
+
+        private void DropGestureRecognizer_Drop(Object sender, DropEventArgs e)
+        {
+            var label = (Label)((Element)sender).Parent;
+            var dropLabel = (Label)e.Data.Properties["Label"];
+            if (label == dropLabel)
+                return;
+
+            Debug.WriteLine($"DropGestureRecognizer_Drop [{dropLabel.Text}] => [{label.Text}]");
+
+            var sourceContainer = (Grid)dropLabel.Parent;
+            var targetContainer = (Grid)label.Parent;
+            sourceContainer.Children.Remove(dropLabel);
+            targetContainer.Children.Remove(label);
+            sourceContainer.Children.Add(label);
+            targetContainer.Children.Add(dropLabel);
+
+            e.Handled = true;
+        }
+
+        private void DragGestureRecognizer_DragStarting_Collection(System.Object sender, Xamarin.Forms.DragStartingEventArgs e)
+        {
+
+        }
+
+        private void DropGestureRecognizer_Drop_Collection(System.Object sender, Xamarin.Forms.DropEventArgs e)
+        {
+            // We handle reordering login in our view model
+            e.Handled = true;
         }
     }
 }
