4f40d8eab692823c599e3043be9db3f5d21ace12
Virendra <virendra.thakur31@gmail.com>
2021-07-31 19:52:42 +0530

    Notification model updated.

---
+++ SampleApp.FreshMVVM/SampleApp.FreshMVVM/Models/Notification.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace SampleApp.FreshMVVM.Models
+{
+    public class Notification
+    {
+        public long NotificationId { get; set; }
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public DateTime ReceivedDateTime { get; set; }
+        public string ImageURL { get; set; }
+        public bool IsRead { get; set; }
+        public bool IsFavourite { get; set; }
+        public bool IsDeleted { get; set; }
+        public bool IsSelected { get; set; }
+        public string ExtraParameterFirst { get; set; }
+        public string ExtraParameterSeccond { get; set; }
+    }
+}
